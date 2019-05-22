b8ed6a68111d3330f3942f273fc6ddf06224b26c
Roman S <rashkur@gmail.com>
2019-05-22 12:16:59 +0200

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -4,6 +4,8 @@ WORKDIR=$(pwd)/shared
 DOMAIN=docker.rashkur.com
 EMAIL=rashkur@gmail.com
 
+mkdir -p "$WORKDIR"/certbot
+
 docker run --rm -it -v "$WORKDIR"/certbot:/etc/letsencrypt -p 80:80 certbot/certbot certonly --authenticator standalone -d "$DOMAIN" -m "$EMAIL" --agree-tos -n
 
 openssl dhparam -dsaparam -out "$WORKDIR/nginx/ssl-dhparams.pem" 4096
